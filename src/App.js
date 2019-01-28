186afb8b5ac3600159d5961d5628157bd0c194c0
Pravin + Ajit <pravin.lokhande@cuelogic.com>
2019-01-28 19:26:50 +0530

    master store redux thunk with devtool extension setup done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import createStore from './store/masterStore';
+
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
+  	console.log('*******************');
+  	console.log('this.props', this.props);
+  	const store = createStore(this.props);
     return (
-      <BrowserRouter>
-	      <div className="App">
-	        <Blog />
-	      </div>
-      </BrowserRouter>
+    	<Provider store={ store }>
+    		<BrowserRouter>
+		        <div className="App">
+		            <Blog />
+		        </div>
+	        </BrowserRouter>
+    	</Provider>
     );
   }
 }
